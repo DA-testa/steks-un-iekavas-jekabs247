980a7c2b29a251c9131b6fd831e9d8ee80b2a402
jekabs247 <93982525+jekabs247@users.noreply.github.com>
2023-02-19 13:21:30 +0000

    test 1

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
-# python3
+
+# Jēkabs Kindzulis, 221RDC047, 18.gr
 
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-
 def find_mismatch(text):
     opening_brackets_stack = []
+
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack:
+                return i+1
+            else:
+                temp = opening_brackets_stack.pop()
+                if not are_matching(temp[0], next):
+                    return i+1
 
+    if opening_brackets_stack:
+        return opening_brackets_stack[0][1]+1
+
+    return "Success"
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
 
+    text = input()
+
+    n = len(text)
+    if n < 1 or n > 105:
+        print("Invalid input length. Length must be between 1 and 105.")
+        return
+
+    mismatch = find_mismatch(text)
+
+    # Printing answer, write your code here
+    print(mismatch)
 
 if __name__ == "__main__":
     main()
